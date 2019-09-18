cc3962ccccc089d76a8a508070eacc999ee673e7
Hearty Oh <heartyoh@hatiolab.com>
2019-09-18 18:22:22 +0800

    v0.1.2

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,10 +1,12 @@
-import { registerEditor, registerRenderer } from '@things-factory/grist-ui'
+import { html } from 'lit-html'
+import { store, navigate } from '@things-factory/shell'
+import { registerEditor, registerRenderer, DataGrist, getEditor } from '@things-factory/grist-ui'
 
 import { IdRenderer } from './renderers/id-renderer'
 import { IdEditor } from './editors/id-editor'
 import { BarcodeRenderer } from './renderers/barcode-renderer'
 import { i18next } from '@things-factory/i18n-base'
-import { DataGrist, getEditor } from '@things-factory/grist-ui'
+import { ADD_MORENDA } from '@things-factory/more-base'
 
 export default function bootstrap() {
   /* global setting for DataGrist */
@@ -14,4 +16,18 @@ export default function bootstrap() {
   registerRenderer('barcode', BarcodeRenderer)
   registerEditor('id', IdEditor)
   registerEditor('barcode', getEditor('string'))
+
+  /* add user profile morenda */
+  store.dispatch({
+    type: ADD_MORENDA,
+    morenda: {
+      icon: html`
+        <mwc-icon>list</mwc-icon>
+      `,
+      name: 'grist test',
+      action: () => {
+        navigate('grist-test')
+      }
+    }
+  })
 }
