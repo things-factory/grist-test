3bb96572ebd8dd7d534ddf63dbb20db383263354
Hearty Oh <heartyoh@hatiolab.com>
2019-09-20 15:25:22 +0800

    v0.1.8

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,16 +1,15 @@
 import { html } from 'lit-html'
 import { store, navigate } from '@things-factory/shell'
-import { registerEditor, registerRenderer, DataGrist, getEditor } from '@things-factory/grist-ui'
+import { registerEditor, registerRenderer, getEditor } from '@things-factory/grist-ui'
 
 import { IdRenderer } from './renderers/id-renderer'
 import { IdEditor } from './editors/id-editor'
 import { BarcodeRenderer } from './renderers/barcode-renderer'
-import { i18next } from '@things-factory/i18n-base'
 import { ADD_MORENDA } from '@things-factory/more-base'
 
 export default function bootstrap() {
   /* global setting for DataGrist */
-  DataGrist.translator = x => i18next.t(x)
+  // DataGrist.translator = x => i18next.t(x)  // We don't use DataGrist.translator
 
   registerRenderer('id', IdRenderer)
   registerRenderer('barcode', BarcodeRenderer)
